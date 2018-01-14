2125a219a8323ed544e38d206dde0926716e4677
JB <jeppeb91@hotmail.com>
2018-01-14 21:11:03 +0100

    Seek from the highest probability working

--- Doubly_linked_list/Doubly_linked_list.c
+++ Doubly_linked_list/Doubly_linked_list.c
@@ -1,43 +1,100 @@
 #include <stdlib.h>
 #include <unistd.h>
-
-typedef struct{
+#include <stdio.h>
+typedef struct queue{
 	int size;
-	long average;
-	struct node *last;
-	struct node *first;
+	float average;
+	struct node* last;
+	struct node* first;
 }queue;
 
-struct node{
+typedef struct node{
 	int prio;
 	int val;
-	struct node *next;
-	struct node *prev;
-};
+	struct node* next;
+	struct node* prev;
+}node;
+node* make_node(){
+	node* n = (node*)malloc(sizeof(node));
+	n-> prio = 0;
+	n->val = 0;
+	n->next = NULL;
+	n->prev = NULL;
+	return n;
+}
+queue* make_queue(){
+	queue* q = (queue*)malloc(sizeof(queue));
+	q->size = 0;
+	q->average = 0;
+	q->first = NULL;
+	q->last = NULL;	
+	return q;
+}
+void insert(queue* q, int priority, int value){
+	node* e = make_node();
+	e->prio = priority;
+	e->val = value;	
+	int old_size = q->size;	
+	int new_size = old_size + 1;	
+	float old_average = q->average;
+	if(q->last == NULL){
+		//If there are no nodes in queue				
+		q->first = e;
+		q->last = e;
+	}else{				
+		printf("%f\n", old_average);		
+		if(priority>old_average){
+			printf("%f\n", old_average);			
+			//Seek from highest prio
+			node* current = q->last;			
+			while(current->prio >= priority){				
+				printf("%d\n", current->prio);											
+				current = current->prev;			
+				if(current->prev == NULL){
+					//If no node with lower					
+					e->next = current;					
+					e->prev = make_node();					
+					q->first = e;				
+					break;
+				}					
+			}
+			if(current == q->last){					
+				//if none after				
+				current->next = e;				
+				e->prev = q->last;
+				q->last = e;						
+			}else{
+				//If its supposed to go between two
+				e->next = current->next;				
+				node* after_e = current->next;					
+				after_e->prev = e;				
+				current->next = e;				
+				e->prev = current;											
+			 }
+		}	
+	}	
 
+	float new_average = (float)(old_size*old_average + priority)/new_size;
+	q->average = new_average;
+	q->size = new_size;	
+}
+void iterqueue(node* n){
+	//This method iterates through and prints a queue from its last element to its first
+	//For testing purposes only	
+	printf("%d : %d\n", n->prio, n->val);
+	if(n->prev == NULL){
+		return;
+	}	
+	iterqueue(n->prev);
+}
 int main(){
-	struct node n_0;
-	struct node n_1;
-	struct node n_2;	
-
-	n_0.prio = 1;
-	n_1.prio = 2;
-	n_2.prio = 3;	
-	
-	n_0.next = &n_1;
-	n_0.prev = NULL;
-
-	n_1.next = &n_2;
-	n_1.prev = &n_1;
-
-	n_2.next = NULL;
-	n_2.prev = &n_1;				
-	queue q;
-	
-	q.first = &n_0;
-	q.last= &n_2;
-	
-	int i = 0;	
-	printf("%d\n", n_2.prio);
+	int ITERATIONS = 100;	
+	queue* q = make_queue();		
+	for(int i = 1; i < ITERATIONS; i++){
+		insert(q, i, i);
+	}	
+	insert(q, 51, 1337);	
+	node* o = q->last;	
+	iterqueue(o);		
 	return 0;
 }
