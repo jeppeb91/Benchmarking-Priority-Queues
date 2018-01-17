cff3f58bfe35d9fbefe4a1319e9b12a87867dcd2
GaPhil <gaphil.io@gmail.com>
2018-01-17 14:22:16 +0100

    WIP skew heaps

--- Skew_heap/Skew_heap.c
+++ Skew_heap/Skew_heap.c
@@ -13,6 +13,7 @@ typedef struct node{
 	struct node* left;
 	struct node* right;
 }node;
+
 node* make_node(){
 	node* n = (node*)malloc(sizeof(node));
 	n-> prio = 0;
@@ -21,15 +22,51 @@ node* make_node(){
 	n->right = NULL;
 	return n;
 }
+
 queue* make_queue(){
 	queue* q = (queue*)malloc(sizeof(queue));
-	q->size = 0;	
+	q->size = 0;
 	q->root = NULL;
 	return q;
 }
 
+node* meld2(node* p, node* q) {
+	node* temp = NULL;
+//	if(p == NULL && q == NULL) return NULL;
+//	if(p == NULL) return q;
+//	if(q == NULL) return p;
+	if(p->prio <= q->prio) {
+		temp = p->right;
+		p->right = q;
+		meld2(temp, q);
+	}else{
+		temp = q->right;
+		q->right = p;
+		meld2(temp, p);
+	}
+	return NULL;
+}
+
+void meldTest() {
+	node* p1 = make_node();
+	node* p2 = make_node();
+	node* q1 = make_node();
+	node* q2 = make_node();
+	p1->prio = 1;
+	p2->prio = 10;
+	q1->prio = 5;
+	q2->prio = 12;
+	p1->right = p2;
+	q1->right = q2;
+	node* test = meld2(p1, q1);
+	while(test != NULL){
+		printf("%f\n", test->prio);
+		test = test->right;
+	}
+}
+
 void meld(node* p, node* q){
-	//ger just nu falska minsta	
+	//ger just nu falska minsta
 	node* subroot = NULL;	
 	node* temp = NULL;			
 	if(p->prio <= q->prio){
@@ -84,6 +121,7 @@ int main(){
 	m->prio = 2;
 	o->prio = 3;	
 	n->right = m;
-	meld(n, o);
+//	meld(n, o);
+	meldTest();
 	return 0;
 }
