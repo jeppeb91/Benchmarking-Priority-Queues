a86a930af132b2b15c328ef4ecbd5a4914471361
JB <jeppeb91@hotmail.com>
2018-02-04 18:45:28 +0100

    Redefined bench to test avg and worst at the same time

--- Bench_marking/badass.sh
+++ Bench_marking/badass.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 #Run benchmarks with highest priorities using specified input file and output data
-sudo nice -n -20 ./bin/skew_heap_inserts "dists/unif09-11.txt" > out/skew/inserts_unif09-11.dat
-sudo nice -n -20 ./bin/skew_heap_inserts "dists/triangular.txt" > out/skew/inserts_triangular.dat
-sudo nice -n -20 ./bin/skew_heap_inserts "dists/exponential1.txt" > out/skew/inserts_exponential.dat
+sudo nice -n -20 ./bin/skew_heap_inserts "dists/unif09-11.txt" #> out/skew/inserts_unif09-11.dat
+sudo nice -n -20 ./bin/skew_heap_inserts "dists/triangular.txt" #> out/skew/inserts_triangular.dat
+sudo nice -n -20 ./bin/skew_heap_inserts "dists/exponential1.txt" #> out/skew/inserts_exponential.dat
 
 
 #Run benchmarks with highest priorities using specified input file and output data
-sudo nice -n -20 ./bin/linked_list_inserts "dists/unif09-11.txt" > out/linked/inserts_unif09-11.dat
-sudo nice -n -20 ./bin/linked_list_inserts "dists/triangular.txt" > out/linked/inserts_triangular.dat
-sudo nice -n -20 ./bin/linked_list_inserts "dists/exponential1.txt" >out/linked/inserts_exponential.dat
+sudo nice -n -20 ./bin/linked_list_inserts "dists/unif09-11.txt" #> out/linked/inserts_unif09-11.dat
+sudo nice -n -20 ./bin/linked_list_inserts "dists/triangular.txt" #> out/linked/inserts_triangular.dat
+sudo nice -n -20 ./bin/linked_list_inserts "dists/exponential1.txt" #>out/linked/inserts_exponential.dat
 
 
 #Run benchmarks with highest priorities using specified input file and output data
-sudo nice -n -20 ./bin/skew_heap_pops "dists/unif09-11.txt" > out/skew/pops_unif09-11.dat
-sudo nice -n -20 ./bin/skew_heap_pops "dists/triangular.txt" > out/skew/pops_triangular.dat
-sudo nice -n -20 ./bin/skew_heap_pops "dists/exponential1.txt" > out/skew/pops_exponential.dat
+sudo nice -n -20 ./bin/skew_heap_pops "dists/unif09-11.txt" #> out/skew/pops_unif09-11.dat
+sudo nice -n -20 ./bin/skew_heap_pops "dists/triangular.txt" #> out/skew/pops_triangular.dat
+sudo nice -n -20 ./bin/skew_heap_pops "dists/exponential1.txt" #> out/skew/pops_exponential.dat
 
 #Run benchmarks with highest priorities using specified input file and output data
-sudo nice -n -20 ./bin/linked_list_pops "dists/unif09-11.txt" > out/linked/pops_unif09-11.dat
-sudo nice -n -20 ./bin/linked_list_pops "dists/triangular.txt" > out/linked/pops_triangular.dat
-sudo nice -n -20 ./bin/linked_list_pops "dists/exponential1.txt" > out/linked/pops_exponential.dat
+sudo nice -n -20 ./bin/linked_list_pops "dists/unif09-11.txt" #> out/linked/pops_unif09-11.dat
+sudo nice -n -20 ./bin/linked_list_pops "dists/triangular.txt" #> out/linked/pops_triangular.dat
+sudo nice -n -20 ./bin/linked_list_pops "dists/exponential1.txt" #> out/linked/pops_exponential.dat
 
 #Plot the results
 
@@ -35,12 +35,31 @@ gnuplot << EOF
 	set xrange [0:1000]
 	set yrange [0:200]
 
-	set output './graphs/Skew_inserts.png'
-	set title "Benchmarking Skew heap inserts"
+	set output './graphs/Skew_inserts_avg.png'
+	set title "Average case of skew heap inserts"
 
-	plot	"./out/skew/inserts_triangular.dat" u 1:2 w lines title "triangular", \
-		"./out/skew/inserts_exponential.dat" u 1:2 w lines title "exponential", \
-		"./out/skew/inserts_unif09-11.dat" u 1:2 w lines title "uniform0.9-1.1"
+	plot	"./out/skew/triangular.txt_inserts_avg.dat" u 1:2 w lines title "triangular", \
+		"./out/skew/exponential1.txt_inserts_avg.dat" u 1:2 w lines title "exponential", \
+		"./out/skew/unif09-11.txt_inserts_avg.dat" u 1:2 w lines title "uniform0.9-1.1"
+EOF
+
+gnuplot << EOF
+
+	set terminal png
+	set key left top
+
+	set xlabel "Queue size"
+	set ylabel"Insertion time (µs)"
+
+	set xrange [0:1000]
+	set yrange [0:200]
+
+	set output './graphs/Skew_inserts_worst.png'
+	set title "Worst case of skew heap inserts"
+
+	plot	"./out/skew/triangular.txt_inserts_worst.dat" u 1:2 w lines title "triangular", \
+		"./out/skew/exponential1.txt_inserts_worst.dat" u 1:2 w lines title "exponential", \
+		"./out/skew/unif09-11.txt_inserts_worst.dat" u 1:2 w lines title "uniform0.9-1.1"
 EOF
 
 gnuplot << EOF
@@ -54,12 +73,32 @@ gnuplot << EOF
 	set xrange [0:1000]
 	set yrange [0:300]
 
-	set output './graphs/Linked_list_inserts.png'
-	set title "Benchmarking Linked list inserts"
+	set output './graphs/Linked_list_inserts_worst.png'
+	set title "Worst case of doubly linked list inserts"
 
-	plot	"./out/linked/inserts_triangular.dat" u 1:2 w lines title "triangular", \
-		"./out/linked/inserts_exponential.dat" u 1:2 w lines title "exponential", \
-		"./out/linked/inserts_unif09-11.dat" u 1:2 w lines title "uniform0.9-1.1"
+	plot	"./out/linked/triangular.txt_inserts_worst.dat" u 1:2 w lines title "triangular", \
+		"./out/linked/exponential1.txt_inserts_worst.dat" u 1:2 w lines title "exponential", \
+		"./out/linked/unif09-11.txt_inserts_worst.dat" u 1:2 w lines title "uniform0.9-1.1"
+	
+EOF
+
+gnuplot << EOF
+	
+	set terminal png
+	set key left top
+
+	set xlabel "Queue size"
+	set ylabel"Insertion time (µs)"
+
+	set xrange [0:1000]
+	set yrange [0:300]
+
+	set output './graphs/Linked_list_inserts_avg.png'
+	set title "Average case of doubly linked list inserts"
+
+	plot	"./out/linked/triangular.txt_inserts_avg.dat" u 1:2 w lines title "triangular", \
+		"./out/linked/exponential1.txt_inserts_avg.dat" u 1:2 w lines title "exponential", \
+		"./out/linked/unif09-11.txt_inserts_avg.dat" u 1:2 w lines title "uniform0.9-1.1"
 	
 EOF
 
@@ -75,14 +114,14 @@ gnuplot << EOF
 	set yrange [0:300]
 
 	set output './graphs/Insertion_comparison.png'
-	set title "Linked list vs Skew heap insertions"
+	set title "Average insertion time Linked list vs Skew heap"
 
-	plot	"./out/linked/inserts_triangular.dat" u 1:2 w lines title "linked triangular", \
-		"./out/linked/inserts_exponential.dat" u 1:2 w lines title "linked exponential", \
-		"./out/linked/inserts_unif09-11.dat" u 1:2 w lines title "linked uniform0.9-1.1", \
-		"./out/skew/inserts_triangular.dat" u 1:2 w lines title "skew triangular", \
-		"./out/skew/inserts_exponential.dat" u 1:2 w lines title "skew exponential", \
-		"./out/skew/inserts_unif09-11.dat" u 1:2 w lines title "skew uniform0.9-1.1"	
+	plot	"./out/linked/triangular.txt_inserts_avg.dat" u 1:2 w lines title "linked triangular", \
+		"./out/linked/exponential1.txt_inserts_avg.dat" u 1:2 w lines title "linked exponential", \
+		"./out/linked/unif09-11.txt_inserts_avg.dat" u 1:2 w lines title "linked uniform0.9-1.1", \
+		"./out/skew/triangular.txt_inserts_avg.dat" u 1:2 w lines title "skew triangular", \
+		"./out/skew/exponential1.txt_inserts_avg.dat" u 1:2 w lines title "skew exponential", \
+		"./out/skew/unif09-11.txt_inserts_avg.dat" u 1:2 w lines title "skew uniform0.9-1.1"	
 EOF
 
 
--- Bench_marking/linked_list_inserts.c
+++ Bench_marking/linked_list_inserts.c
@@ -2,8 +2,21 @@
 //#include "lib/Skew_heap.h"
 #include <time.h>
 #include <sys/time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "lib/priority_reader.h"
-
+char* parse_out_path(char* dist, char* filename)
+{
+	int counter = 0;	
+	char * base_path = "./out/linked/";
+	char *result = malloc(strlen(base_path)+strlen(&dist[6])+strlen(filename)+1);
+	//in real code you would check for errors in malloc here
+	strcpy(result, base_path);
+	strcat(result, &dist[6]);
+	strcat(&result[strlen(&dist[6]) + strlen(base_path)], filename);
+	return result;
+}
 void iteration(float* rands, int iteration_no, unsigned long* rounds){
 	queue* q = make_queue();	
 	struct timeval start;
@@ -36,27 +49,47 @@ void iteration(float* rands, int iteration_no, unsigned long* rounds){
 	free(q);	
 	free(nodes);	
 }
-void print_avg(unsigned long* rounds, int repeats){
+void print_avg(unsigned long* rounds, int repeats, FILE* fp){
 	unsigned long sum = 0;	
 	for(int i = 0; i < 975; i++){
 		sum = 0;
 		for(int j = 0; j < repeats; j++){
 			sum = sum + rounds[i + 975*j];		
 		}		
-		printf("%d\t%lu\n", i + 25, sum/repeats); 	
+		fprintf(fp, "%d\t%lu\n", i + 25, sum/repeats); 	
 	}
 }
-void bench(int repeats, float* rands){
+void print_worst(unsigned long* rounds, int repeats, FILE* fp){
+	unsigned long worst = 0;	
+	for(int i = 0; i < 975; i++){
+		worst = rounds[i];
+		for(int j = 0; j < repeats; j++){			
+			if(rounds[i + 975 * j] > worst){
+				worst = rounds[i + 975 * j];
+			}		
+		}		
+		fprintf(fp, "%d\t%lu\n", i + 25, worst); 	
+	}
+}
+void bench(int repeats, float* rands, char* dist){
 	unsigned long* rounds = malloc(sizeof(unsigned long)*975*repeats);
 	for(int i = 0; i < repeats; i++){
-		iteration(rands, i, rounds);		
+		iteration(rands, i, rounds);
 	}
-	print_avg(rounds, repeats);
+	char * path = parse_out_path(dist, "_inserts_worst.dat");
+	FILE* fp = fopen(path, "w");
+	print_worst(rounds, repeats, fp);
+	fclose(fp);
+	
+	path = parse_out_path(dist, "_inserts_avg.dat");
+	fp = fopen(path, "w");
+	print_avg(rounds, repeats, fp);
+	fclose(fp);
 }
 int main(int argc, char **argv){
 	unsigned long* rounds = malloc(sizeof(unsigned long)*975*10);
 	float* rands = read_priorities(argv[1]);
-	bench(100, rands);
+	bench(5, rands, argv[1]);
 	return 0;
 }
 
